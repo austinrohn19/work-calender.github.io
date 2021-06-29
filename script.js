9975add8d3db5256c5d1cddfb497ab513045c06c
Austin Rohn <AustinRohn19@gmail.com>
2021-06-28 21:26:11 -0400

    updated the HTML and created my const hours

---
+++ script.js
@@ -0,0 +1,11 @@
+const hours= [
+    {display:"9am", value:8},
+    {display:"10am", value:9},
+    {display:"11am", value:10},
+    {display:"12pm", value:11},
+    {display:"1pm", value:12},
+    {display:"2pm", value:13},
+    {display:"3pm", value:14},
+    {display:"4pm", value:15},
+    {display:"5pm", value:16}
+];

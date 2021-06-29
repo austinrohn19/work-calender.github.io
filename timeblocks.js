dbfb7310a64a382ae67c7f43f728a52d165ad7ba
Austin Rohn <AustinRohn19@gmail.com>
2021-06-28 22:02:12 -0400

    finished timeblocks I think???

--- timeblocks.js
+++ timeblocks.js
@@ -1,30 +1,34 @@
 function createtimeblock(time, info, save) {
     const timeblockEL =$(`
-    <div class="row time-block">
-        <div class="hour">${time.display}</div>
-    </div>
-`);
+        <div class="row time-block">
+            <div class="hour">${time.display}</div>
+        </div>
+    `);
 
-    const timeblockInput = $(`
-    <textarea name="time-block" class="description">${info}</textarea>
-`);
+        const timeblockInput = $(`
+        <textarea name="time-block" class="description">${info}</textarea>
+    `);
 
-const currentHour= moment().hour();
-    if (time.value < currenthour) timeblockInput.addClass("past");
+    const currentHour= moment().hour();
+        if (time.value < currenthour) timeblockInput.addClass("past");
 
-   else if (time.value === currenthour) timeblockInput.addClass("present");
+        else if (time.value === currenthour) timeblockInput.addClass("present");
 
-   else timeblockInput.addClass("future"):
+        else timeblockInput.addClass("future"):
 
-   const timeblockSaveBtn = (`
-    <button class="saveBtn">
-        <i class="fas fa-Save"></i>
-    </button>
-`);
+    const timeblockSaveBtn = (`
+        <button class="saveBtn">
+            <i class="fas fa-Save"></i>
+        </button>
+    `);
 
-timeblockSaveBtn.click(function () {
-    timeblocks[save] = timeblockInput.val();
-    localStorage.setItem("timeblocks", JSON.stringify(timeblocks));
-});
+    timeblockSaveBtn.click(function () {
+        timeblocks[save] = timeblockInput.val();
+        localStorage.setItem("timeblocks", JSON.stringify(timeblocks));
+    });
 
+    timeblockEL.append(timeblockInput);
+    timeblockEL.append(timeblockSaveBtn);
+
+    return timeblockEL;
 }
